9e1a7ac09cc450f573aa316fc6496833fc93631c
Guilhermy <133382489+guilhermy17@users.noreply.github.com>
2024-09-02 13:23:14 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const prompt = require('prompt-sync');
+const entrada = prompt();
+
+var senha = entrada('Digite a senha: ');
+var senhaCorreta = '2024';
+
+
+while (senha != senhaCorreta) {
+senha +-  parseInt(senhaCorreta);
+senha = parseInt(entrada('Acesso negado. Tente novamente:'));
+}
+
+console.log('Acesso permitido');
